f6032531459b4a0a5f1c7e916050eba695171d9d
lfortizma <lfortizma@gmail.com>
2021-10-01 17:10:08 -0500

    Modificación Botón Venta
    Cambios a la funcionalidad del botón venta

--- Sprint_2/javascript/actualizarventa.js
+++ Sprint_2/javascript/actualizarventa.js
@@ -1,18 +1,15 @@
 window.onload = iniciar;
 
+function iniciar() {
+  
+    var btnGuardar = document.getElementById("btnGuardar");
+    btnGuardar.addEventListener("click", clickBtnGuardar);
 
- 
-  function iniciar() {
-   
-    var btnGuardar = document.getElementById("btnGuardarActualizacion");
-    btnGuardarActualizacion.addEventListener("click", clickBtnGuardarActualizacion);
- 
 }
 
-  function clickBtnGuardarActualizacion(){
-    if(document.getElementById("inputForm")){
-      alert("se actualizo el registro")
-      
+function clickBtnGuardar() {
+  if (document.getElementById('switch').checked){
+  
+  alert("Registo Guardado")
     }
-    
 }
