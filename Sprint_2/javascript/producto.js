e1f68e0badb3e2082d45385ea076c3bd85546d27
PaulaAmado <90434956+PaulaAmado@users.noreply.github.com>
2021-10-01 15:23:11 -0500

    Añadí botón notificación exitosa

---
+++ Sprint_2/javascript/producto.js
@@ -0,0 +1,17 @@
+window.onload = iniciar;
+
+
+ 
+  function iniciar() {
+   
+    var Notificar = document.getElementById("Notificar");
+     Notificar.addEventListener("click", clicknotificar);
+ 
+}
+
+  function clicknotificar(){
+    
+      alert("Los cambios han sido guardados")
+      
+    }
+    
