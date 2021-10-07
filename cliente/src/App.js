b912ed3b8fba9c1766dfc6d249e8bba60a326b2d
LuisGuerra <lcgg18@gmail.com>
2021-10-07 10:47:51 -0500

    Se agrego la cliente react

---
+++ cliente/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Usuarios from './Pages/Usuarios';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       <Usuarios/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
